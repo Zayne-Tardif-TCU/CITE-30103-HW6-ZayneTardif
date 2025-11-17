538fb53743f9841fa5c5024d7693481fc75c2ff9
Zayne_Tardif_TCU_ <Zayne.h.tardif@tcu.edu>
2025-11-16 20:38:56 -0600

    Answer for Question 19

---
+++ question19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i = 1, sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    while (i <= n)
+    {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum from 1 to %d = %d\n", n, sum);
+
+    return 0;
+}
