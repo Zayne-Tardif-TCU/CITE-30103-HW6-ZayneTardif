732b6fa7013c883ca6282e8277823a5b5f89908d
Zayne_Tardif_TCU_ <Zayne.h.tardif@tcu.edu>
2025-11-16 20:06:22 -0600

    Answer for Question 16

---
+++ question16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n > 0)
+        printf("%d is positive.\n", n);
+    else if (n < 0)
+        printf("%d is negative.\n", n);
+    else
+        printf("The number is zero.\n");
+
+    return 0;
+}
